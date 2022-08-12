e72eb20b632dd7eda0f3c7b51550dbb4899fe3dc
Victor Mu <victor@helium.com>
2022-08-11 17:09:44 -0700

    Change create invite url

--- ui/src/views/tenants/CreateTenantUser.tsx
+++ ui/src/views/tenants/CreateTenantUser.tsx
@@ -44,7 +44,7 @@ class CreateTenantUser extends Component<IProps, IState> {
   onFinish = async (obj: TenantUser) => {
     // Highjack the request to login server
     if (this.state.user) {
-      const res = await fetch(window.location.origin + "/api/invitations", {
+      const res = await fetch(`http://${window.login_app_url}/api/invitations`, {
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
